5da181c8dd0cb19d395d622f82b3fa5f3ea685b2
Pasha <pasha@2vega.com>
2018-03-28 00:49:58 -0700

    Updated lldb script

--- scripts/lldb/remote_inspector.py
+++ scripts/lldb/remote_inspector.py
@@ -4,44 +4,58 @@ import os
 import subprocess
 import lldb
 import json
+import requests
 
 def __lldb_init_module(debugger, internal_dict):
-    debugger.HandleCommand("command script add -f " + __name__ + ".remote_inspect ri")
-    debugger.HandleCommand("command script add -f " + __name__ + ".remote_reset rr")
-    debugger.HandleCommand("command script add -f " + __name__ + ".print_json pj")
     debugger.HandleCommand("command script add -f " + __name__ + ".remote_trace rt")
-    debugger.HandleCommand("command script add -f " + __name__ + ".log_to_file lf")
     rpc_call("reset", None)
     
-    print 'Use "ri" command in the same manner as you would po, to inspect the object via remote inspector.'
-
-def remote_inspect(debugger, command, result, internal_dict):
-    frame = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
-    value = frame.EvaluateExpression("print " + command).summary
-    #value = eval_print_json(frame, command).summary
-    rpc_call("inspect", [ command, value ])
+    print 'Use "rt" command in the same manner as you would po, to inspect the object via remote inspector.'
     
 def remote_trace(debugger, command, result, internal_dict):
-    frame = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
+    thread = debugger.GetSelectedTarget().GetProcess().GetSelectedThread()
+    frame = thread.GetSelectedFrame()
+    addr = frame.addr
+    ln = addr.line_entry
     
-    #value = frame.EvaluateExpression("thread backtrace all")
-    value = frame.EvaluateExpression("bt")
-    #subprocess.call(["syslog", "-s", "-l", "notice", "HERE HERE HERE")
-    rpc_call("trace", [value.summary])
+    data = {"pc": frame.pc, "stackp": frame.sp, "file": ln.file.fullpath, "line": ln.line}
     
-def remote_reset(debugger, command, result, internal_dict):
-    rpc_call("reset", None)
+    module = frame.module
+    if module.num_symbols > 0:
+        data["module"] = module.symbols[0].name
+    
+    function = frame.function
+    if function.name != None:
+        data["function"] = function.name
+        
+    lang = lldb.SBLanguageRuntime.GetNameForLanguageType(frame.compile_unit.GetLanguage())
+    if lang != None:
+        data["language"] = lang
+        
+    variables = frame.variables
+    varInfos = []
+    for var in variables:
+        varInfos.append({"name": var.name, "type": var.GetTypeName(), "value": var.summary})
+    data["variables"] = varInfos
+    
+    arguments = frame.arguments
+    argInfos = []
+    for arg in arguments:
+        argInfos.append({"name": arg.name, "type": arg.GetTypeName(), "value": arg.summary})
+    data["arguments"] = argInfos
+    
+    rpc_call("trace", data)
+    
+    
+#==== Utils =====#
     
 def rpc_call(method, value):
-    payload = {"jsonrpc": "2.0", "id": 1, "method": method}
-    payload["params"] = value
-    #subprocess.call(["syslog", "-s", "-l", "notice", "curl", "-H", "Content-Type: application/json", "http://localhost:16164/api/jsonrpc", "-d", json.dumps(payload)])
-    subprocess.call(["curl", "-H", "Content-Type: application/json", "http://localhost:16164/api/jsonrpc", "-d", json.dumps(payload,separators=(',',':'))])
-
-def print_json(debugger, command, result, internal_dict):
-    frame = debugger.GetSelectedTarget().GetProcess().GetSelectedThread().GetSelectedFrame()
-    result = eval_print_json(frame, command) 
-    print result.summary
+    payload = {"jsonrpc": "2.0", "id": 1, "method": method, "params": value}
+    jsPayload = json.dumps(payload)
+    print(jsPayload)
+    url = "http://localhost:16164/api/jsonrpc"
+    content_type = "application/json"
+    req = requests.post(url, json=jsPayload)
         
 def eval_print_json(frame, command):
     expr = [
@@ -52,8 +66,3 @@ def eval_print_json(frame, command):
         "print $result"
     ]
     return frame.EvaluateExpression("; ".join(expr))
-
-
-def log_to_file(debugger, command, result, dict):
-  f=open("/tmp/lldb.out","w")
-  debugger.SetOutputFileHandle(f,True)
